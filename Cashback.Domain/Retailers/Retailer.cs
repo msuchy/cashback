18c106455ea28b532678837e899da1836aa251a4
Margareth Suchy <margareths@jmalucelliti.com.br>
2020-06-23 16:03:15 -0300

    Fix filename

--- Cashback.Domain/Retailers/Retailer.cs
+++ Cashback.Domain/Retailers/Retailer.cs
@@ -3,13 +3,15 @@ using Cashback.Domain.Dtos.Retailers;
 
 namespace Cashback.Domain.Retailers
 {
-    public class Retailer{
+    public class Retailer
+    {
         public Cpf CPF { get; private set; }
         public string Name { get; private set; }
         public Email Email { get; private set; }
         public string Password { get; private set; }
 
-        public Retailer (CreateRetailerDto retailerInfo){
+        public Retailer(CreateRetailerDto retailerInfo)
+        {
             CPF = new Cpf(retailerInfo.CPF);
             Name = retailerInfo.Name;
             Email = new Email(retailerInfo.Email);
